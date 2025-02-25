becb5668e52a20068808c5348cc1c7dead49fec7
RTCupid <19apr2006@vk.com>
2025-02-25 14:14:22 +0300

    working code, showing registers to int 08h

--- regframe.asm
+++ regframe.asm
@@ -10,6 +10,7 @@
 org 100h
 
 Start:
+;---------------intercept-interrupt-09h----------------------------------------
                 xor  ax, ax                     ; ax = 0
                 mov  es, ax                     ; es = ax
                 mov  bx, 09h * 4                ; offset for ptr to ISR_09h
@@ -19,10 +20,6 @@ Start:
                 mov  ax, es:[bx + 2]            ; ptrs to interrupt service
                 mov  Seg_old_09h, ax            ; routine
 
-                mov  al, 0ah
-                call PrintOneHexNumber          ; print hex al
-
-
                 int  09h                        ; call old ISR 09h
 
                 cli
@@ -30,11 +27,33 @@ Start:
                                                 ; service routiny
                 push cs
                 pop  ax                         ; ax = cs
-                mov  es:[bx + 2], ax            ; es:[bx + 2] = ax (= segment
-                                                ; with code)
+                mov  es:[bx + 2], ax            ; segment of my interrupt 08h
                 sti
 
                 int  09h                        ; call old ISR 09h
+
+;---------------intercept-interrupt-08h----------------------------------------
+
+                mov  bx, 08h * 4                ; offset for ptr to ISR_08h
+
+                mov  ax, es:[bx]                ; Ofs_old_09h and Seg_old_09h
+                mov  Ofs_old_08h, ax            ; from array with
+                mov  ax, es:[bx + 2]            ; ptrs to interrupt service
+                mov  Seg_old_08h, ax            ; routine
+
+                int  08h
+
+                cli
+                mov  es:[bx], offset MY_ISR_08h ; offset of my interrupt 08h
+                                                ; service routiny
+                push cs
+                pop  ax                         ; ax = cs
+                mov  es:[bx + 2], ax            ; segment of my interrupt 08h
+                sti
+
+                int  08h
+
+;---------------end-of-intercept-----------------------------------------------
 
                 mov  ax, 3100h                  ; DOS Fn 31H: Terminate & Stay
                                                 ; Resident
@@ -78,16 +97,16 @@ MY_ISR_09h      proc
                 mov  di, (80 - 18) * 2          ;-----------------
                 call MakeFrame                  ; Make frame for registers
 
-                pop  ax                         ;------------------------
-                pop  bx                         ; return parrent value  |
-                pop  cx                         ; of registers          |
-                pop  dx                         ;------------------------
-                push dx                         ;------------------------
-                push cx                         ; save ax bx cx dx in   |
-                push bx                         ; stack again           |
-                push ax                         ;------------------------
+                ;pop  ax                         ;------------------------
+                ;pop  bx                         ; return parrent value  |
+                ;pop  cx                         ; of registers          |
+                ;pop  dx                         ;------------------------
+                ;push dx                         ;------------------------
+                ;push cx                         ; save ax bx cx dx in   |
+                ;push bx                         ; stack again           |
+                ;push ax                         ;------------------------
 
-                call ShowRegisters              ; Show info about registers
+                ;call ShowRegisters              ; Show info about registers
 
                 jmp  NotPressG                  ; goto NotPressG
 
@@ -134,13 +153,61 @@ Seg_old_09h     dw   0                          ; segment
 MY_ISR_09h      endp
 
 ;------------------------------------------------------------------------------
+; MY_ISR_08h - new handler for interrupt 08h
+; Entry:        None
+; Exit:         None
+; Destroy:      None
+;------------------------------------------------------------------------------
+MY_ISR_08h      proc
+                nop
+                nop
+                nop
+                nop
+                push si                         ; save si in stack
+                push di                         ; save di in stack
+                push es                         ; save es in stack
+                push ds                         ; save ds in stack
+                push dx                         ; save dx in stack
+                push cx                         ; save cx in stack
+                push bx                         ; save bx in stack
+                push ax                         ; save ax in stack
+
+                push cs
+                pop  ds                         ; ds = cs
+
+                call ShowRegisters              ; Show info about registers
+
+                ;mov  al,  20h                   ; al = 20h
+                ;out  20h, al                    ; out to interrupt controller
+
+                pop  ax                         ; back ax from stack
+                pop  bx                         ; back bx from stack
+                pop  cx                         ; back cx from stack
+                pop  dx                         ; back dx from stack
+                pop  ds                         ; back ds from stack
+                pop  es                         ; back es from stack
+                pop  di                         ; back di from stack
+                pop  si                         ; back si from stack
+
+                db   0eah                       ; jmp
+Ofs_old_08h     dw   0                          ; offset  old ISR_08h
+Seg_old_08h     dw   0                          ; segment old ISR_08h
+                nop
+                nop
+                nop
+                nop
+                nop
+                iret                            ; interrupt return
+MY_ISR_08h      endp
+
+;------------------------------------------------------------------------------
 ; ShowRegisters Func to show information about registers
 ; Entry:        ax - parrent value of ax
 ;               bx - parrent value of bx
 ;               cx - parrent value of cx
 ;               dx - parrent value of dx
 ; Exit:         None
-; Destroy:      si, cx, di, es
+; Destroy:      si, di, es, ax, bx, cx, dx
 ;------------------------------------------------------------------------------
 ShowRegisters   proc
                 push ds                         ; save ds in stack
